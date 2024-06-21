38db7cb03d0db99d2db478c72d65afc226330d90
Maikel Wever <maikelwever@gmail.com>
2024-06-21 19:12:27 +0200

    Flip debug mode based on environment variable

--- sony-pm-alt.py
+++ sony-pm-alt.py
@@ -1,4 +1,5 @@
 #!/usr/bin/python
+import os
 import socket, struct, time, socketserver, re, subprocess, sys, logging, logging.handlers
 import _thread, requests, os
 from threading import Thread
@@ -22,7 +23,7 @@ CUSTOM_LD_LIBRARY_PATH = "/usr/local/lib" #common path if self-compiled
 #Might want to change:
 PHOTO_DIR = "/var/lib/Sony"  #photo/videos will be downloaded to here
 PTP_GUID = "ff:ff:52:54:00:b6:fd:a9:ff:ff:52:3c:28:07:a9:3a" #default value for sony-guid-setter
-DEBUG = True
+DEBUG = bool(os.environ.get("DEBUG", False))
 #------------------------------------------------------------------
 
 #replace '~' if used
